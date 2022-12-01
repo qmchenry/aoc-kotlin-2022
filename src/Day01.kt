d217b54ff8efef10d830fbeaf99602d3b9c84275
Quinn McHenry <quinn.mchenry@smallplanet.com>
2022-12-01 12:29:35 -0500

    Add Day 1 solution for advent of code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
+
+    fun sums(input: List<String>): List<Int> {
+        return input
+            .flatMapIndexed { index, x ->
+                when {
+                    index == 0 || index == input.lastIndex -> listOf(index)
+                    x.isEmpty() -> listOf(index - 1, index + 1)
+                    else -> emptyList()
+                }
+            }
+            .windowed(size = 2, step = 2) { (from, to) ->
+                input.slice(from..to)
+                    .map { it.toInt() }
+            }
+            .map { it.sum() }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return sums(input).maxOf { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return sums(input)
+            .sorted()
+            .takeLast(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_sample")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val realInput = readInput("Day01_test")
+    println("Part 1: ${part1(realInput)}")
+    println("Part 2: ${part2(realInput)}")
 }
